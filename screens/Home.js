1bd0b443f658787abd9ac4c99e4da2d45acbe13b
Farhan Farooq <farhan.farooq@live.com>
2021-03-22 13:55:43 +0500

    search ads by title

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { View, FlatList } from "react-native";
+import { TextInput } from "react-native-paper";
 import Card from "../components/Card";
 import firebase from "../fbconfig";
 
 const Home = () => {
   const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+
+  const searchQuery = items?.filter((item) =>
+    item.title.toLowerCase().includes(text.toLowerCase())
+  );
 
   useEffect(() => {
     const unsub = firebase
@@ -26,10 +32,19 @@ const Home = () => {
 
   return (
     <View>
+      <TextInput
+        style={{ marginVertical: 30, paddingHorizontal: 10 }}
+        label="Search"
+        autoCapitalize="none"
+        mode="outlined"
+        value={text}
+        onChangeText={(e) => setText(e)}
+      />
       <FlatList
-        data={items}
+        data={searchQuery}
         keyExtractor={(item) => item.key}
         renderItem={({ item }) => <Card item={item} />}
+        contentContainerStyle={{ paddingBottom: 120 }}
       />
     </View>
   );
